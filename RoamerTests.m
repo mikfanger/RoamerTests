070f2ad88aeb6888dba2241fe5dfea75a6169eec
mikfanger <mikfanger@gmail.com>
2014-10-01 18:43:30 -0500

    Roamer Test
    Roamer test files

---
+++ RoamerTests.m
@@ -0,0 +1,34 @@
+//
+//  RoamerTests.m
+//  RoamerTests
+//
+//  Created by Mac Home on 2/21/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RoamerTests : XCTestCase
+
+@end
+
+@implementation RoamerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
